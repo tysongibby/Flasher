a8731ca792d49af90f43127ff2daa76bdd389803
tysongibby <tyson.gibby@outlook.com>
2021-08-25 14:38:59 -0600

    remove non functioning projects

---
+++ Flasher/Flasher.Api/Controllers/FlashCardController.cs
@@ -0,0 +1,45 @@
+﻿using Flasher.Server.Data.Repositories.Interfaces;
+using Flasher.Shared;
+using Flasher.Shared.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flasher.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class FlashCardController : ControllerBase
+    {
+        private readonly ILogger<FlashCardController> _logger;
+        private readonly IFlashCardRepository _flashCardRepository;
+
+        public FlashCardController(ILogger<FlashCardController> logger, IFlashCardRepository flashCardRepository)
+        {
+            _logger = logger;
+            _flashCardRepository = flashCardRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<FlashCard> Get(int id)
+        {
+            var flashCardItem = _flashCardRepository.Get(id);
+            return Ok(flashCardItem);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<FlashCard>> GetAll()
+        {
+            var flashCardItems = _flashCardRepository.GetAll();
+            return Ok(flashCardItems);
+        }
+
+
+
+
+    }
+
+}
