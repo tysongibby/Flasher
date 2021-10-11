686fae8ae35aff602f2cc0f19f890a67f5f789ee
tyson.gibby <tyson.gibby@outlook.com>
2021-10-11 09:59:01 -0600

    updated data migration

--- Flasher/FlasherApi/Migrations/20211011155046_addedTitleToFlashCard.Designer.cs
+++ Flasher/FlasherApi/Migrations/20211011155046_addedTitleToFlashCard.Designer.cs
@@ -2,14 +2,16 @@
 using Flasher.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FlasherApi.Migrations
 {
     [DbContext(typeof(FlasherContext))]
-    partial class FlasherContextModelSnapshot : ModelSnapshot
+    [Migration("20211011155046_addedTitleToFlashCard")]
+    partial class addedTitleToFlashCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
