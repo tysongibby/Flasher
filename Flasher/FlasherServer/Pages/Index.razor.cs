63a39be158aff612795b6f46bef0f861ecf76a2e
tyson.gibby <tyson.gibby@outlook.com>
2021-12-02 21:29:16 -0700

    created rough of index page (start page)

--- Flasher/FlasherServer/Pages/Index.razor.cs
+++ Flasher/FlasherServer/Pages/Index.razor.cs
@@ -4,10 +4,10 @@ using AutoMapper;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using FlasherData.Models;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace FlasherServer.Pages
 {
-
     public partial class Index
     {
         [Inject]
@@ -18,25 +18,28 @@ namespace FlasherServer.Pages
         private Superset SelectedSuperset { get; set; } = new Superset();
         private IList<Set> Sets { get; set; } = new List<Set>();
         private Set SelectedSet { get; set; } = new Set();
+        private int SelectedSupersetId { get; set; } = 0;
+        private int SelectedSetId { get; set; } = 0;        
+        private bool SupersetSelectDisabled { get; set; } = true;
        
 
         protected override void OnInitialized()
         {
-            SelectedSuperset.Id = 0;
-            SelectedSet.Id = 0;
-
             IList<SupersetModel> _supersetModels = UnitOfWork.Supersets.GetAll();
             foreach(SupersetModel _supersetModel in _supersetModels)
             {
                 Supersets.Add(Mapper.Map<Superset>(_supersetModel));
-            }
+            }            
+        }
 
+        private void HandleOnValidSubmit()
+        {            
             IList<SetModel> _setModels = UnitOfWork.Sets.GetAll();
-            foreach(SetModel _setModel in _setModels)
+            foreach (SetModel _setModel in _setModels)
             {
                 Sets.Add(Mapper.Map<Set>(_setModel));
             }
-            
+            SupersetSelectDisabled = false;
         }
     }
 }
