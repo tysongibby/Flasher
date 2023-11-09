2fe0dc13198ed664c7bb4c43da402b6c101d1cfe
Tyson Gibby <tyson.gibby@outlook.com>
2023-11-08 23:19:25 -0700

    Added base WASM project

--- Flasher/FlasherWeb/Program.cs
+++ Flasher/FlasherWeb/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using FlasherWeb.Services;
 using FlasherWeb.Services.Interfaces;
 using FlasherWeb;
-using Microsoft.Extensions.DependencyInjection;
+using FlasherData.DataModels;
+using FlasherData.Context;
+using FlasherData.Repositories;
+using FlasherData.Repositories.Interfaces;
 using System.Net.Http;
 using System;
 
@@ -11,10 +17,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Setup API services
 builder.Services.AddScoped(api => new HttpClient { BaseAddress = new Uri(builder.Configuration["api_base_url"]) });
-builder.Services.AddHttpClient<IFlashcardService, FlashcardService>(fcs => fcs.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
-builder.Services.AddHttpClient<ISubjectService, SubjectService>(sss => sss.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
-builder.Services.AddHttpClient<ICategoryService, CategoryService>(ss => ss.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
+builder.Services.AddHttpClient<IFlashcardService, FlashcardService>(fs => fs.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
+builder.Services.AddHttpClient<ISubjectService, SubjectService>(ss => ss.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
+builder.Services.AddHttpClient<ICategoryService, CategoryService>(cs => cs.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
+
+// Setup database services
+builder.Services.AddDbContext<FlasherContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("FlasherDb")));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 await builder.Build().RunAsync();
 
